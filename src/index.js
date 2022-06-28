e91f032da31c2675735fcb06b23d218ae822492a
Emanuele Panesi <emanuele.panesi@gmail.com>
2022-06-28 12:53:00 +0200

    Created functions to connect server and database

---
+++ src/index.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const debug = require("debug")("roadtripmap:root");
+
+const chalk = require("chalk");
+const connectDB = require("./database");
+const initializeServer = require("./server/initializeServer");
+
+const port = process.env.PORT ?? 4000;
+const connectionString = process.env.DATABASE_MONGO;
+
+(async () => {
+  try {
+    await connectDB(connectionString);
+    await initializeServer(port);
+  } catch {
+    debug(chalk.red("Exiting with errors"));
+    process.exit(1);
+  }
+})();
