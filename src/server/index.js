5d1d9c68e5629130b4013afa87c6b01e25e2a666
Emanuele Panesi <emanuele.panesi@gmail.com>
2022-07-11 18:37:28 +0200

    Added cors middleware

--- src/server/index.js
+++ src/server/index.js
@@ -1,14 +1,21 @@
 const express = require("express");
+const cors = require("cors");
 const morgan = require("morgan");
 const locationRouter = require("../routers/locationRouter");
 const userRouter = require("../routers/userRouter");
+const { notFoundError, generalError } = require("./middlewares/errors");
+const corsOptions = require("../utils/corsOptions");
 
 const app = express();
 
+app.use(cors(corsOptions));
 app.use(morgan("dev"));
 app.use(express.json());
 
 app.use("/user", userRouter);
 app.use("/locations", locationRouter);
+
+app.use(notFoundError);
+app.use(generalError);
 
 module.exports = app;
---
+++ src/utils/corsOptions.js
@@ -0,0 +1,11 @@
+const corsOptions = {
+  origin: [
+    "http://localhost:4000",
+    "http://localhost:3000",
+    "http://localhost:4005",
+    "https://emanuele-panesi-front-final-project-202204-bcn.netlify.app",
+    "https://emanuele-panesi-front-final-project-202204-bcn.netlify.app/",
+  ],
+};
+
+module.exports = corsOptions;
